35699107781b47623c0245996878f9953d19beb5
Anemoiaa <georgiypetroff@gmail.com>
2025-07-26 16:15:53 +0400

    feat: implement TextRowRepository

---
+++ app/Repositories/TextRowRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\TextRow;
+use Illuminate\Support\Collection;
+
+class TextRowRepository
+{
+    public function getAll(): Collection
+    {
+        return TextRow::all();
+    }
+
+    public function deleteAll(): void
+    {
+        TextRow::query()->delete();
+    }
+
+    public function generate(int $count): void
+    {
+        $half = (int) floor($count / 2);
+        $rest = $count - ($half * 2);
+
+        $allowed = TextRow::factory()->count($half)->allowed()->make()->toArray();
+        $prohibited = TextRow::factory()->count($half)->prohibited()->make()->toArray();
+
+        $data = array_merge($allowed, $prohibited);
+
+        if ($rest > 0) {
+            $data[] = TextRow::factory()->allowed()->make()->toArray();
+        }
+
+        TextRow::insert($data);
+    }
+}
