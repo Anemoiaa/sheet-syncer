c30b44fffc74f5a7ac5432139bd678fbb53b3982
Anemoiaa <georgiypetroff@gmail.com>
2025-07-28 12:15:13 +0400

    fix: use Validator facade

--- app/Http/Controllers/TextRowController.php
+++ app/Http/Controllers/TextRowController.php
@@ -11,8 +11,9 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Validation\Validator;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\View\View;
+
 
 class TextRowController extends Controller
 {
