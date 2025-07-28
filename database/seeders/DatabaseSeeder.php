135aaae9cc2effa4d9000e2e049246ead32fdacf
Anemoiaa <georgiypetroff@gmail.com>
2025-07-28 12:02:26 +0400

    fix: code-style

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\SheetConfig;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -21,7 +20,7 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
-           SheetConfigSeeder::class,
+            SheetConfigSeeder::class,
         ]);
     }
 }
